e1c918c28f2562126a7ad9bbf42445f07ab889ab
laptop <laptop@scy.com>
2020-03-13 17:23:20 +0800

    update test

--- tests/test_rule.py
+++ tests/test_rule.py
@@ -15,83 +15,83 @@ class TestRule(unittest.TestCase):
         content = '变化'
         last_content = ''
         res = is_changed(rule, content, last_content)
-        self.assertTrue(res)
+        self.assertEqual(res, 3)
 
     def test_no_rule(self):
         rule = ''
         content = '变化'
         last_content = '变化'
         res = is_changed(rule, content, last_content)
-        self.assertFalse(res)
+        self.assertEqual(res, 0)
 
         rule = ''
         content = '变化'
         last_content = '不变化'
         res = is_changed(rule, content, last_content)
-        self.assertTrue(res)
+        self.assertEqual(res, 3)
 
     def test_contains(self):
         rule = '-contain 变化'
         content = '变化'
         last_content = '不变化'
         res = is_changed(rule, content, last_content)
-        self.assertTrue(res)
+        self.assertEqual(res, 2)
 
         rule = '-contain 变化'
         content = '你好'
         last_content = '不变化'
         res = is_changed(rule, content, last_content)
-        self.assertFalse(res)
+        self.assertEqual(res, 1)
 
     def test_increase(self):
         rule = '-increase 3'
         content = '1888.1'
         last_content = '1885.1'
         res = is_changed(rule, content, last_content)
-        self.assertFalse(res)
+        self.assertEqual(res, 1)
 
         rule = '-increase 3'
         content = '1888.2'
         last_content = '1885.1'
         res = is_changed(rule, content, last_content)
-        self.assertTrue(res)
+        self.assertEqual(res, 2)
 
         rule = '-increase 0'
         content = '1888.1'
         last_content = '1888.1'
         res = is_changed(rule, content, last_content)
-        self.assertFalse(res)
+        self.assertEqual(res, 0)
 
         rule = '-increase 0'
         content = '1888.2'
         last_content = '1888.1'
         res = is_changed(rule, content, last_content)
-        self.assertTrue(res)
+        self.assertEqual(res, 2)
 
     def test_decrease(self):
         rule = '-decrease 3'
         content = '1882.1'
         last_content = '1885.1'
         res = is_changed(rule, content, last_content)
-        self.assertFalse(res)
+        self.assertEqual(res, 1)
 
         rule = '-decrease 3'
         content = '1882'
         last_content = '1885.1'
         res = is_changed(rule, content, last_content)
-        self.assertTrue(res)
+        self.assertEqual(res, 2)
 
         rule = '-decrease 0'
         content = '1888.1'
         last_content = '1888.1'
         res = is_changed(rule, content, last_content)
-        self.assertFalse(res)
+        self.assertEqual(res, 0)
 
         rule = '-decrease 0'
         content = '1888.0'
         last_content = '1888.1'
         res = is_changed(rule, content, last_content)
-        self.assertTrue(res)
+        self.assertEqual(res, 2)
 
 
 if __name__ == '__main__':
