66ccb8127352d6125bb6966551b1249de458cf01
scy_laptop <835410808@qq.com>
2020-04-29 10:37:04 +0800

    fix database locked

--- webmonitor/settings.py
+++ webmonitor/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+            'timeout': 20,
+        }
     }
 }
 
