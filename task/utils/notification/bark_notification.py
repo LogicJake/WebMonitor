901820ec6e93cf26c864afd1c8d470e0210d6628
Richard Xue <yueyericardo@gmail.com>
2020-12-17 05:40:51 -0500

    add url to bark

--- task/utils/notification/bark_notification.py
+++ task/utils/notification/bark_notification.py
@@ -1,7 +1,7 @@
 
 import json
 import logging
-
+import re
 import requests
 
 from task.utils.notification.notification import Notification
@@ -10,12 +10,22 @@ import urllib.parse
 logger = logging.getLogger('main')
 
 
+def getUrlQuery(content):
+    regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
+    urls = re.findall(regex, content)
+    if len(urls):
+        url = [x[0] for x in urls][0]
+        url_query = f'?url={urllib.parse.quote_plus(url)}'
+        return url_query
+    return ''
+
+
 class BarkNotification(Notification):
     def send(self, to, header, content):
         if to == '默认':
             logger.error('没有设置Bark KEY，无法发送Bark通知')
             raise Exception('没有设置Bark KEY，无法发送Bark通知')
-        url = 'https://api.day.app/{}/{}/{}'.format(to, header, urllib.parse.quote_plus(content))
+        url = 'https://api.day.app/{}/{}/{}{}'.format(to, header, urllib.parse.quote_plus(content), getUrlQuery(content))
         r = requests.post(url)
 
         res = json.loads(r.text)
