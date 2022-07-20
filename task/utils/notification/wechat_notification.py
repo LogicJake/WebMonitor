408b28d1007303b8a40feb13e53446be19525344
AlbertShenC <shen_hw9903@163.com>
2022-07-20 23:32:44 +0800

    修改Server酱接口至新版

--- task/utils/notification/wechat_notification.py
+++ task/utils/notification/wechat_notification.py
@@ -15,9 +15,9 @@ class WechatNotification(Notification):
             logger.error('没有设置Server酱 SCKEY，无法发送微信通知')
             raise Exception('没有设置Server酱 SCKEY，无法发送微信通知')
         data = {'text': header, 'desp': content}
-        url = 'https://sc.ftqq.com/{}.send'.format(to)
+        url = 'https://sctapi.ftqq.com/{}.send'.format(to)
         r = requests.post(url, data=data)
 
         res = json.loads(r.text)
-        if res['errno'] != 0:
-            raise Exception(res['errmsg'])
+        if res['data']['errno'] != 0:
+            raise Exception(res['data']['errmsg'])
